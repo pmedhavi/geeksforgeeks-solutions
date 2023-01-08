b4ffed8277c9edbcc8f0ae3ca9ea399d7cdbeaea
Prakhar Medhavi <medhhaviprakhar@gmail.com>
2023-01-08 17:02:22 +0530

    Create check_string.java

---
+++ check_string.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            String s = sc.next ();
+            if (new Sol().check (s))
+                System.out.println ("YES");
+            else
+                System.out.println ("NO");
+        }
+        
+    }
+}
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Sol
+{
+    Boolean check (String s)
+    {
+        // your code here      
+        char[] ch=s.toCharArray();
+        int l=ch.length;
+        for(int i=0;i<l;i++){
+            if(ch[0]!=ch[i])
+            return false;
+        }
+        return true;
+    }
+}
