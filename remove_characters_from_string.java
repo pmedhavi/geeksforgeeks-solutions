4336d1ecbdd6fdac6f2d0afad8479f96258868f8
Prakhar Medhavi <medhhaviprakhar@gmail.com>
2023-01-08 19:35:36 +0530

    Create remove_characters_from_string.java

---
+++ remove_characters_from_string.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String s = read.readLine();
+            
+            Solution ob = new Solution();
+            String result = ob.removeCharacters(s);
+            
+            System.out.println(result);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    String removeCharacters(String S) {
+        // code here
+        String s1="";
+        for(int i=0;i<S.length();i++){
+            if(S.charAt(i)>=48&&S.charAt(i)<=57){
+                s1+=S.charAt(i);
+            }
+        }
+        return s1;
+    }
+}
